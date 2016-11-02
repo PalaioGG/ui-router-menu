13c03ea68b22c1897b4c847a31fbafbc4ed31ce6
Giorgos Palaiologos <geo.pal@hotmail.com>
2016-11-02 20:05:51 +0200

    Update server.js comments

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname, '/'))); //Register the directory
-app.use('/views', express.static(path.join(__dirname, 'views'))); //Register the views folder
-app.use('/assets', express.static(path.join(__dirname, 'assets'))); //Register the assets folder
-app.use('/node_modules', express.static(path.join(__dirname, 'node_modules'))); //Register the node_modules folder
+//Register the directory (get access to index.js)
+app.use(express.static(path.join(__dirname, '/')));
+//Register the views folder
+app.use('/views', express.static(path.join(__dirname, 'views'))); 
+//Register the assets folder
+app.use('/assets', express.static(path.join(__dirname, 'assets'))); 
+//Register the node_modules folder
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules'))); 
 
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
